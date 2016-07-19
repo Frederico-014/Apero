6fe11c3bf743e84c7be29553b0a18e5d3f38769e
Frederic <gomes_frederic@ymail.com>
2016-07-19 13:34:11 +0200

    modification nom de la table apero

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,19 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
+        'username' => $faker->name,
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Apero::class, function (Faker\Generator $faker) {
+    return [
+        'Category_id' => rand(1,3),
+        'title' => $faker->text(50),
+        'abstract' => $faker->text(100),
+        'content' => $faker->text(1000),
+        'date'=>$faker->dateTime,
     ];
 });
