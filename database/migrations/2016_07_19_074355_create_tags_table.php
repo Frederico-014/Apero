fe378dc92167006ad0f30da7f30316885dfae555
Frederic <gomes_frederic@ymail.com>
2016-07-19 10:46:28 +0200

    Migration des tables fini

--- database/migrations/2016_07_19_074355_create_tags_table.php
+++ database/migrations/2016_07_19_074355_create_tags_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,10 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',255);
-            $table->text('description');
-            $table->dateTime('date');
+            $table->integer('count_apero');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('categories');
+        Schema::drop('tags');
     }
 }
