d35527b3b07419e5b2aa108f7910ac82ee0794d1
Frederic <gomes_frederic@ymail.com>
2016-07-19 16:33:55 +0200

    creation views, unitialistation gulp + rajout realation model user->apero

--- app/User.php
+++ app/User.php
@@ -23,5 +23,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
+    public function aperos ()
+    {
+        return $this->hasMany('App\Apero');
+    }
 }
