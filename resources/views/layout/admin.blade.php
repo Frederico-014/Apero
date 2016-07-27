d2d158e107ca754afd148b2bbcc50da91c400612
Frederic <gomes_frederic@ymail.com>
2016-07-27 10:11:56 +0200

    Depart de la partie back

---
+++ resources/views/layout/admin.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Apero</title>
+    <link rel="stylesheet" href="{{url('assets/css/app.min.css')}}">
+</head>
+<body>
+
+<header>
+    <h1>Admin du site</h1>
+    <nav>
+        <h2>
+            <a href="{{url('')}}">Accueil</a>
+            <p>hello admin</p>
+            <a href="{{url('logout')}}">Se deconnecter</a>
+        </h2>
+    </nav>
+</header>
+
+<div class="containeur grid-2-1">
+    <section>
+        @yield('content')
+    </section>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="{{url('assets/js/app.min.js')}}"></script>
+</body>
+</html>
